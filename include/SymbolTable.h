beb8629b577705715a2f11925197ca6afb2bf308
DaOnlyOwner <jan@hemming-haas.eu>
2019-12-16 16:08:48 +0100

    Update SymbolTable.h

--- include/SymbolTable.h
+++ include/SymbolTable.h
@@ -29,19 +29,19 @@ class SymbolTable
 public:
 	SymbolTable();
 
-	bool add(Variable&& var) { return m_variables.Add(var.name, std::move(var)) > 0; }
+	bool add(Variable&& var) { return m_variables.Add(var.name,std::move(var)) > 0; }
 	bool add(Function&& fn);
-	bool add(MetaType&& mt) { return m_meta_types.Add(mt.get_name(), std::move(mt)); }
+	bool add(MetaType&& mt) { return m_meta_types.Add(mt.get_name(), std::move(mt)) > 0; }
 
 	std::pair<Variable*, size_t> get_var(const std::string& name) { auto ret = m_variables.Get(name); return std::make_pair(ret.first, ret.second); }
 	Variable& get_var(const UID& uid) { return m_variables.GetByIndex(uid.index); }
 
-	std::pair<std::vector<Function>*,size_t> get_funcs(const std::string& name) { auto ret = m_functions.Get(name); return std::make_pair(ret.first, ret.second); }
+	std::pair<std::vector<Function>*,size_t> get_funcs(const std::string& name) { return m_functions.Get(name);}
 	// TODO: Performance: predefined operations on predefined types (e.g. 1+3) should be in a static table for faster lookup. For example a static (and modified SymbolTable class) called predefined_symbols, that doesn't have string as key etc. 
 	std::vector<Function>& get_funcs(const UID& uid) { return m_functions.GetByIndex(uid.index); }
 
-	// TODO: Performance: When primitive type, first lookup in a static table, just loke in get_funcs.
-	std::pair<MetaType*,size_t> get_meta_type(const std::string& name) {auto ret = m_meta_types.Get(name); return std::make_pair(ret.first, ret.second); }
+	// TODO: Performance: When primitive type, first lookup in a static table, just look in get_funcs.
+	std::pair<MetaType*,size_t> get_meta_type(const std::string& name) {return m_meta_types.Get(name);}
 	MetaType& get_meta_type(const UID& uid) { return m_meta_types.GetByIndex(uid.index); }
 	
 private:
