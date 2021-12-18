8e464c27ba868fcb54e0d14d932b34754c9f5a3d
DaOnlyOwner <jan@hemming-haas.eu>
2021-12-18 01:15:33 +0100

    Fixed compiler driver

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,32 @@ int execute(int argc, char** argv)
 {
 	cxxopts::Options options("Hyc", "The compiler for Hyc");
 	options.add_options()
-		("O", "Optimization level (0-3 or z)", cxxopts::value<std::string>()->default_value("1"))
+		("O,opt", "Optimization level (0-3 or z)", cxxopts::value<std::string>()->default_value("1"))
 		("e,emit", "Emit (specify 'ir' for IR output or 'obj' for object file output)", cxxopts::value<std::string>()->default_value("obj"))
-		("tt,target_triple", "The target triple that should be targeted", cxxopts::value<std::string>());
-	auto result = options.parse(argc, argv);
+		("t,target_triple", "The target triple that should be targeted", cxxopts::value<std::string>())
+		("f,file", "The input file", cxxopts::value<std::string>())
+		("h,help", "Print the help text");
+	
+	options.parse_positional({ "f" });
+	//options.allow_unrecognised_options();
+	cxxopts::ParseResult result;
+	try
+	{
+		result = options.parse(argc, argv);
+	}
+
+	catch (cxxopts::missing_argument_exception e)
+	{
+		fmt::print(fmt::fg(fmt::color::orange_red),"{}", e.what());
+		return 1;
+	}
+
+	if (result.count("h") >= 1)
+	{
+		fmt::print("{}",options.help());
+		return 0;
+	}
+
 	if (result.count("O") > 1)
 	{
 		fmt::print(fmt::fg(fmt::color::orange_red), "Too many arguments given for -O option");
@@ -22,7 +44,7 @@ int execute(int argc, char** argv)
 		fmt::print(fmt::fg(fmt::color::orange_red), "Too many arguments given for -e (--emit) option");
 		return 1;
 	}
-
+	
 	auto opt = result["O"].as<std::string>();
 	if (opt != "0" && opt != "1" && opt != "2" && opt != "3" && opt != "z")
 	{
@@ -39,16 +61,16 @@ int execute(int argc, char** argv)
 	};
 	LLVMBackend::CompilerInfo ci;
 	ci.opt_lvl = to_opt_level[opt];
-	if (result.count("tt") == 0)
+	if (result.count("t") == 0)
 	{
 		ci.target = std::nullopt;
 	}
 	else
 	{
-		ci.target = std::make_optional(result["tt"].as<std::string>());
+		ci.target = std::make_optional(result["t"].as<std::string>());
 	}
 
-	auto ei = result["ei"].as<std::string>();
+	auto ei = result["e"].as<std::string>();
 	if (ei == "ir")
 	{
 		ci.emit_info = LLVMBackend::CompilerInfo::EmitInfo::EmitIRCode;
@@ -60,14 +82,14 @@ int execute(int argc, char** argv)
 		return 1;
 	}
 
-	if (argc == 1)
+	if (result.count("f") == 0)
 	{
 		fmt::print(fmt::fg(fmt::color::orange_red), "No file specified", ei);
 		return 1;
 	}
 
 	Pipeline pl;
-	return pl.build(argv[1], ci);
+	return pl.build(result["f"].as<std::string>(), ci);
 }
 // TODO: Correctness: Handle user errors
 
